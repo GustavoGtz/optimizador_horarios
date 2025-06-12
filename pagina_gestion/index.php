0e792fa92f4b0a037da7f95a217102727f03c525
GustavoLaptop <gustavogtznav@gmail.com>
2025-06-12 10:57:31 -0700

    Base del CRUD

---
+++ pagina_gestion/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>BUAP</title>
+</head>
+<body>
+<h1>Proyecto de Optimización para Calendarios Escolares</h1>
+<h2> Gestiones </h2>
+<a href="unidad_academico/index.php">Unidades Academicas</a>
+<br>
+<a href="programa_educativo/index.php">Programas Educativos</a>
+<br>
+<a href="tipo_clase/index.php">Tipos de Clase</a>
+<br>
+<a href="materia/index.php">Materias</a>
+<br>
+<a href="edificio/index.php">Edificios</a>
+<br>
+<a href="aula/index.php">Aulas</a>
+<br>
+<a href="contrato/index.php">Contratos</a>
+<br>
+<a href="profesor/index.php">Profesores</a>
+<br>
+<a href="profesor_materia/index.php">Profesores y sus Materias</a>
+</body>
+</html>
---
+++ pagina_gestion/sql/tabla.sql
@@ -0,0 +1,69 @@
+CREATE TABLE Unidad_Academica (
+    id_unidad_academica SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    abreviatura VARCHAR(5)
+);
+
+CREATE TABLE Tipo_Clase (
+    id_tipo_clase SERIAL PRIMARY KEY,
+    nombre VARCHAR(100)
+);
+
+CREATE TABLE Contrato (
+    id_contrato SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    horas_minimas INT NOT NULL,
+    horas_maximas INT NOT NULL
+);
+
+CREATE TABLE Programa_Educativo (
+    id_programa_educativo SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    abreviatura VARCHAR(5),
+    id_unidad_academica INT NOT NULL,
+    FOREIGN KEY (id_unidad_academica) REFERENCES Unidad_Academica(id_unidad_academica)
+);
+
+CREATE TABLE Materia (
+    id_materia SERIAL PRIMARY KEY,
+    clave VARCHAR(20),
+    nombre VARCHAR(100),
+    creditos INT NOT NULL,
+    semestre INT NOT NULL,
+    horas_por_semana INT NOT NULL,
+    id_tipo_clase INT NOT NULL,
+    id_programa_educativo INT NOT NULL,
+    FOREIGN KEY (id_tipo_clase) REFERENCES Tipo_Clase(id_tipo_clase),
+    FOREIGN KEY (id_programa_educativo) REFERENCES Programa_Educativo(id_programa_educativo)
+);
+
+CREATE TABLE Profesor (
+    id_profesor INT NOT NULL PRIMARY KEY,
+    nombre VARCHAR(100),
+    id_contrato INT NOT NULL,
+    FOREIGN KEY (id_contrato) REFERENCES Contrato(id_contrato)
+);
+
+CREATE TABLE Profesor_Materia (
+    id_profesor INT NOT NULL,
+    id_materia INT NOT NULL,
+    PRIMARY KEY (id_profesor, id_materia),
+    FOREIGN KEY (id_profesor) REFERENCES Profesor(id_profesor),
+    FOREIGN KEY (id_materia) REFERENCES Materia(id_materia)
+);
+
+CREATE TABLE Edificio (
+    id_edificio SERIAL PRIMARY KEY,
+    nombre VARCHAR(100)
+);
+
+CREATE TABLE Aula (
+    id_aula INT NOT NULL,
+    id_edificio INT NOT NULL,
+    id_tipo_clase INT NOT NULL,
+    cupo INT NOT NULL,
+    PRIMARY KEY (id_aula, id_edificio),
+    FOREIGN KEY (id_edificio) REFERENCES Edificio(id_edificio),
+    FOREIGN KEY (id_tipo_clase) REFERENCES Tipo_Clase(id_tipo_clase)
+);
+
