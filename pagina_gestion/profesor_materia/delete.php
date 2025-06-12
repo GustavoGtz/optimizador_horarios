be6c1b394b493b49e5e1bd59787edfd6cbff27a7
David Durazo <133311335+daviddurazo@users.noreply.github.com>
2025-06-12 12:48:33 -0600

    Update delete.php
    Codigo adaptado a Profesor_Materia

--- pagina_gestion/profesor_materia/delete.php
+++ pagina_gestion/profesor_materia/delete.php
@@ -1,14 +1,20 @@
 <?php
 require '../config/db.php';
 
-$id = $_GET['id'];
+$id_profesor = $_GET['id_profesor'];
+$id_materia = $_GET['id_materia'];
 
 try {
-    $stmt = $pdo->prepare("DELETE FROM aula WHERE id_aula = :id");
-    $stmt->execute(['id' => $id]);
+    $stmt = $pdo->prepare("DELETE FROM Profesor_Materia WHERE id_profesor = :id_profesor AND id_materia = :id_materia");
+    $stmt->execute([
+        'id_profesor' => $id_profesor,
+        'id_materia' => $id_materia
+    ]);
     header("Location: index.php");
+    exit;
 } catch (PDOException $e) {
-    echo "<h3>❌ No se puede eliminar el aula porque tiene ejemplares asociados.</h3>";
-    echo "<p>Primero elimina o reubica los ejemplares dentro de esta aula.</p>";
+    echo "<h3>❌ No se puede eliminar la asignación porque tiene dependencias asociadas.</h3>";
+    echo "<p>Primero asegúrate de que esta relación no esté siendo usada en otra parte del sistema.</p>";
     echo "<a href='index.php'>Volver</a>";
 }
+?>
