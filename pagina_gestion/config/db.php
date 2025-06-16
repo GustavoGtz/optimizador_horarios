f462e98325f15782005403535f6f95d532528fd0
GustavoLaptop <gustavogtznav@gmail.com>
2025-06-16 09:23:04 -0700

    Script Completo y otros archivos de config

---
+++ pagina_gestion/config/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$db = "buap";
+$user = "postgres";
+$password = "1234";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?>
